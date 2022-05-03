60e95c90841b974b26fc8b8cb32fb2d12602a25d
Pranjal Chaudhary <pranjal.c.05@gmail.com>
2022-05-03 12:27:48 +0530

    task

---
+++ RabbitMQ-Consumer/src/test/java/com/stackroute/RabbitMQConsumer/RabbitMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.RabbitMQConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
